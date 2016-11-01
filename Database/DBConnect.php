7a7fb74e2fc0364cd6a032a830c073a0ffb59cd6
Brett Lockhart <blockhart615@gmail.com>
2016-10-31 17:46:56 -0700

    Fix issues in DBConnect

--- Database/DBConnect.php
+++ Database/DBConnect.php
@@ -1,10 +1,18 @@
 <?php
-    //Configure Database Connection Variables
-    define("DB_HOST", "localhost");
-    define("DB_USER", "root");
-    define("DB_PASSWORD", "liferuined478");
-    define("DB_DATABASE", "SecureChatDB");
+//Configure Database Connection Variables
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "liferuined478");
+define("DB_DATABASE", "SecureChatDB");
 
-    //make connection to database
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+class DBConnect {
+      private $conn;
+
+      public function connect() {
+            //make connection to database
+            $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);            
+
+            return $this->conn;
+      }
+}
 ?>
