b448cde582d2fda79d74802baca324c402bc2d3b
Brett Lockhart <blockhart615@gmail.com>
2016-10-31 17:09:26 -0700

    Fix database names

--- Database/DBFunctions.php
+++ Database/DBFunctions.php
@@ -4,7 +4,7 @@
  * @link http://www.androidhive.info/2012/01/android-login-and-registration-with-php-mysql-and-sqlite/ Complete tutorial
  */
 
-	class DB_Functions {
+	class DBFunctions {
 
 	    private $conn;
 
