f6ac6a6be26088b87b117bfedf5709a2845789eb
PortableUbuntu <blockhart615@gmail.com>
2018-09-14 08:51:38 -0700

    wrap env vars in quotes

--- Server_SecureChat/Database/DBConnect.php
+++ Server_SecureChat/Database/DBConnect.php
@@ -1,9 +1,9 @@
 <?php
 //Configure Database Connection Variables
-define("DB_HOST", getenv('DB_HOST'));
-define("DB_USER", getenv('DB_LOGIN'));
-define("DB_PASSWORD", getenv('DB_PW'));
-define("DB_DATABASE", getenv('DB_NAME'));
+define("DB_HOST", '"' . getenv('DB_HOST') . '"');
+define("DB_USER", '"' . getenv('DB_LOGIN') . '"');
+define("DB_PASSWORD", '"' . getenv('DB_PW') . '"');
+define("DB_DATABASE", '"' . getenv('DB_NAME') . '"');
 
 class DBConnect {
       private $conn;
@@ -11,6 +11,24 @@ class DBConnect {
       public function connect() {
             //make connection to database
             $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+
+            return $this->conn;
+      }
+}
+?>
+
+
+<?php
+
+class DBConnect {
+      private $conn;
+
+      public function connect() {
+            //make connection to database
+            $this->conn = new mysqli(,
+                                     ,
+                                    ,
+                                     );
 
             return $this->conn;
       }
